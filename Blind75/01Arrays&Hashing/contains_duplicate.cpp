3f61730ec8739b038770a13fe69e12d1dd19d44f
SreenathMopuri <44582562+SreenathMopuri@users.noreply.github.com>
2022-07-20 18:16:16 +0530

    Update contains_duplicate.cpp

--- Blind75/01Arrays&Hashing/contains_duplicate.cpp
+++ Blind75/01Arrays&Hashing/contains_duplicate.cpp
@@ -1,6 +1,11 @@
 /*
+    LeetCode Link: https://leetcode.com/problems/contains-duplicate/
+    
     Given int array, return true if any value appears at least twice
-    Ex. nums = [1,2,3,1] -> true, nums = [1,2,3,4] -> false
+    Ex. 
+    Input1: nums = [1,2,3,1] -> true, 
+    Input2: nums = [1,2,3,4] -> false
+    Input3: nums = [1,1,1,3,3,4,3,2,4,2] -> true
     If seen num previously then has dupe, else insert into hash set
     Time: O(n)
     Space: O(n)
